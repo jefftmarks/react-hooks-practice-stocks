a9b6fb724e5e0db7fcdea6d73fb9592e7fcc52ca
Jeff Marks <jeff.t.marks@gmail.com>
2022-08-16 15:40:10 -0400

    done with assignment

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [sortCategory, setSortCategory] = useState("All");
+  const [filterCategory, setFilterCategory] = useState("All");
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then(res => res.json())
+      .then(data => {
+        setStocks(data);
+        setCounter(data.length + 1);
+      })
+  }, [])
+
+  function handleBuyStock(boughtStock) {
+    const updatedStock = {...boughtStock, id: counter};
+    setPortfolio([...portfolio, updatedStock]);
+    setCounter(counter => counter + 1);
+  }
+
+  function handleSellStock(soldStock) {
+    setPortfolio(portfolio.filter(stock => stock.id !== soldStock.id));
+  }
+
+  const updatedStocks = stocks
+    .sort(compareStocks)
+    .filter(filterStocks)
+
+  function compareStocks(a, b) {
+    if (sortCategory === "All") {
+      return 0;
+    } else if (sortCategory === "Alphabetically") {
+      let x = a.ticker.toLowerCase();
+      let y = b.ticker.toLowerCase();
+      if (x > y) return 1;
+      if (x < y) return -1;
+      if (x === y) return 0;
+    } else if (sortCategory === "Price") {
+      return b.price - a.price;
+    }
+  }
+
+  function filterStocks(stock) {
+    if (filterCategory === "All") {
+      return true;
+    } else {
+      return stock.type === filterCategory;
+    }
+  }
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        onSort={setSortCategory}
+        sortCategory={sortCategory}
+        onFilter={setFilterCategory}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={updatedStocks} onBuyStock={handleBuyStock} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer
+            portfolio={portfolio}
+            onSellStock={handleSellStock}
+          />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ onSort, sortCategory, onFilter }) {
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +9,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sortCategory==="Alphabetically" ? true : false}
+          onChange={(e) => onSort(e.target.value)}
         />
         Alphabetically
       </label>
@@ -19,15 +19,16 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sortCategory==="Price" ? true : false}
+          onChange={(e) => onSort(e.target.value)}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(e) => onFilter(e.target.value)}>
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
